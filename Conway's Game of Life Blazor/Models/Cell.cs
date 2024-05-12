14f084b3cb42f18d21be036fea97905310b01af2
Georgi <Georgiivanover@gmail.com>
2024-05-12 11:26:56 +0300

    Separate Grid and Cell classes into Model folder.

---
+++ Conway's Game of Life Blazor/Models/Cell.cs
@@ -0,0 +1,30 @@
+﻿namespace Conway_s_Game_of_Life_Blazor.Models
+{
+        public record Cell
+        {
+            private int row;
+            private int col;
+            private bool isAlive;
+            private bool nextState;
+            public int Row 
+            {
+                get { return row; } 
+                set { row = value; }
+            }
+            public int Col
+            {
+                get { return col; }
+                set { col = value; }
+            }
+            public bool IsAlive
+            {
+                get { return isAlive; }
+                set { isAlive = value; }
+            }
+            public bool NextState
+            {
+                get { return nextState; }
+                set { nextState = value; }
+            }
+    }
+}
---
+++ Conway's Game of Life Blazor/Models/Grid.cs
@@ -0,0 +1,31 @@
+﻿namespace Conway_s_Game_of_Life_Blazor.Models
+{
+    public class Grid
+    {
+        private Cell[,] gameGrid;
+        private int gridX;
+        private int gridY;
+
+        public Grid(int x, int y)
+        {
+            this.GridX = x;
+            this.GridY = y;
+            this.GameGrid = new Cell[GridX, GridY];
+        }
+        public int GridX
+        {
+            get { return gridX; }
+            set { gridX = value; }
+        }
+        public int GridY
+        {
+            get { return gridY; }
+            set { gridY = value; }
+        }
+        public Cell[,] GameGrid 
+        {
+            get { return gameGrid; }
+            set { gameGrid = value; }
+        }
+    }
+}
